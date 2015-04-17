e483caf3280c8e8e920070c8c716abad12a04605
duke605 <duke6605@gmail.com>
2015-04-16 23:40:42 -0500

    Added validations to models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ActiveRecord::Base
+  validates :first_name, :last_name, :address, :city, :postal_code, :email, :province_id, presence: true
+  validates :email, format: { with:  /\A[A-Za-z0-9._%+-]+@[A-Z0-9.-]+\.[A-Za-z]{2,6}\Z/}
+  validates :postal_code, format: { with: /\A([ABCEGHJKLMNPRSTVXY]\d){3}\Z/ }
 end
--- app/models/province.rb
+++ app/models/province.rb
@@ -1,2 +1,4 @@
 class Province < ActiveRecord::Base
+  validates :name, presence: true
+  validates :pst, :gst, :hst, numericality: { greater_than_or_equal_to: .01 }, allow_nil: true
 end
